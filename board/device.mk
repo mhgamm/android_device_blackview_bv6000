06e342cc26cd57e499609a135cfdd8999c2bd607
mhgamm <32566262+mhgamm@users.noreply.github.com>
2018-03-17 21:15:30 +0100

    Correct device name to Blackview

--- board/device.mk
+++ board/device.mk
@@ -1,5 +1,5 @@
 # OTA assert
-TARGET_OTA_ASSERT_DEVICE := bv6000,BV6000,c239v55_dx
+TARGET_OTA_ASSERT_DEVICE := Blackview,s12v55_jk_3m
 
 # Enable Minikin text layout engine (will be the default soon)
 USE_MINIKIN := true
